2af328729bc391ddc8e262502ff9c5fde521b6a9
Darii Yar <dariiy.yar@gmail.com>
2018-03-27 15:28:20 +0300

    Fixes rubocop issues

--- consecutive_prime.rb
+++ consecutive_prime.rb
@@ -13,7 +13,7 @@ class ConsecutivePrime
   private
 
   def boolean_prime_array
-    @is_prime = Array.new(@limit+1, false)
+    @is_prime = Array.new(@limit + 1, false)
     @is_prime[2] = true
     @is_prime[3] = true
     @sqr_lim = Math.sqrt(@limit)
@@ -22,21 +22,21 @@ class ConsecutivePrime
   def find_primes
     x2 = 0
     (1..@sqr_lim).each do |i|
-      x2 += 2 * i-1
-      y2 = 0;
+      x2 += 2 * i - 1
+      y2 = 0
       (1..@sqr_lim).each do |j|
-        y2 += 2 * j-1
+        y2 += 2 * j - 1
         n = 4 * x2 + y2
-        @is_prime[n] = !@is_prime[n] if ((n <= @limit) && (n % 12 == 1 || n % 12 == 5))
+        @is_prime[n] = !@is_prime[n] if (n <= @limit) && (n % 12 == 1 || n % 12 == 5)
         n -= x2
-        @is_prime[n] = !@is_prime[n] if ((n <= @limit) && (n % 12 == 7))
+        @is_prime[n] = !@is_prime[n] if (n <= @limit) && (n % 12 == 7)
         n -= 2 * y2
-        @is_prime[n] = !@is_prime[n] if ((i > j) && (n <= @limit) && (n % 12 == 11))
+        @is_prime[n] = !@is_prime[n] if (i > j) && (n <= @limit) && (n % 12 == 11)
       end
     end
     (5..@sqr_lim).each do |i|
-      if (@is_prime[i])
-        n = i * i;
+      if @is_prime[i]
+        n = i * i
         j = n
         while j <= @limit
           @is_prime[j] = false
@@ -48,21 +48,21 @@ class ConsecutivePrime
 
   def prime_array
     @primes = []
-    @is_prime.each_with_index {|el,i| @primes << i if el }
+    @is_prime.each_with_index {|el, i| @primes << i if el }
   end
 
   def find_consecutive_prime
     @hash = {}
     total_sum = @primes.inject(0, :+)
-    limit = @primes.count-1
-    sum =total_sum
+    limit = @primes.count - 1
+    sum = total_sum
     @hash[:terms_count] = 0
     (0..limit).each do |index|
       ind = limit
-      break if (ind - index +1) < @hash[:terms_count]
+      break if (ind - index + 1) < @hash[:terms_count]
       if sum < @limit && @is_prime[sum]
         result_prime  = sum
-        @hash = {prime: result_prime, terms_count: ind - index +1}
+        @hash = { prime: result_prime, terms_count: ind - index +1 }
         next
       end
       while ind > index
@@ -71,7 +71,7 @@ class ConsecutivePrime
         ind -= 1
         if sum < @limit && @is_prime[sum]
           result_prime  = sum
-          @hash= {prime: result_prime, terms_count: ind - index +1}
+          @hash = { prime: result_prime, terms_count: ind - index +1 }
           break
         end
       end
@@ -84,5 +84,5 @@ class ConsecutivePrime
   end
 end
 
-result = Benchmark.realtime { ConsecutivePrime.new(1000000)}
+result = Benchmark.realtime { ConsecutivePrime.new(1_000_000) }
 puts "Calculating time of the consecutive primes - #{result}s"
